f950b15ec011f097fff79fb7c6fa2d50e1127cbe
Artur Guedes <arturguedx@gmail.com>
2018-11-23 03:54:58 -0200

    Initial files

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# MinimalReplace
+# Minimal Replace
+_Dynamically insert to content into your HTML with `{{::syntax}}` to generate a unified file with the brackets extension. Keep the HTML super clean with Minimal Replace._
+
+[![Version](https://badges.ml/arturguedes.minimaldark/version.svg)](https://brackets-extension-badges.github.io#arturguedes.minimaldark)
+[![Downloads](https://badges.ml/arturguedes.minimaldark/total.svg)](https://brackets-extension-badges.github.io#arturguedes.minimaldark)
+[![GitHub license](https://img.shields.io/github/license/ArturGuedes/MinimalDark.svg)](https://github.com/ArturGuedes/MinimalDark/blob/master/LICENSE)
+[![GitHub last commit](https://img.shields.io/github/last-commit/ArturGuedes/MinimalDark.svg)]()
+
+
+## Getting Started
+There are a few ways to use Minimal Replace: with CDN to LivePreview, using the Brackets Extension, which generates a unified final file or Minimal Replace CDN + Minimal Replace Brackets extension.
+
+### HTML syntax
+Enter any variable with the `{{::name}}` syntax so that it is replaced by the `"name": "Arthur Phillip Dent"` declared inside the javascript
+```html
+<div class="card">
+    <h1>I\'m {{::name}}</h1>
+    <h2>from the {{::planet}}</h2>
+</div>
+```
+
+### Minimal Replace CDN (LivePreview)
+Copy and paste the syntax below into the `<body>` of your HTML
+```javascript
+<script>
+    mr_data = {
+        "name": "Arthur Phillip Dent",
+        "planet": "Earth"
+    }
+</script>
+<script src="https://arturguedes.github.io/MinimalReplace/js/minimalreplace.min.js"></script>
+```
+
+### Minimal Replace Brackets extension (Unified final file)
+Access the `Minimal> Setup Minimal Replace structure` to create the basic structure of files and folders.
+The `mr_data` object is declared inside the javascritp file of the same name as the HTML file
+
+#### Structure
+```
+    .
+    ├── ...
+    ├── mr-structure            # Minimal Replace folder
+    │   ├── example.html        # HTML page containing the expressions {{:: value}}
+    │   ├── example.js          # Javascript with mr_data ={...} object
+    |   └── ...
+    ├── example.html            # Final file unified by HTML and mr_data
+    └── ...
+```
+
+**Note:** The `mr_data` object accepts HTML expressions, but an error occurs when the quotation marks are closed incorrectly. For correct use of the expression: `"link": "<a href="#">Link</a>"` always use backslash before a quote, like this: `"link": "<a href=\"#\">Link</a>"`
+
+### Change History
+* 11/23/2018:
+    * Initial code
+
+## License
+
+This project is licensed under the GNU General Public License v3.0. Read [LICENSE](LICENSE) for further information.
+
+
---
+++ main.js
@@ -0,0 +1,157 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, $, brackets, window */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    // Brackets Modules
+    var CommandManager = brackets.getModule("command/CommandManager"),
+        Menus = brackets.getModule("command/Menus"),
+        ProjectManager = brackets.getModule('project/ProjectManager'),
+        FileSystem = brackets.getModule('filesystem/FileSystem'),
+        FileUtils = brackets.getModule('file/FileUtils'),
+        DocumentManager = brackets.getModule('document/DocumentManager'),
+        DefaultDialogs = brackets.getModule('widgets/DefaultDialogs'),
+        Dialogs = brackets.getModule('widgets/Dialogs'),
+        LiveDevelopment = brackets.getModule('LiveDevelopment/LiveDevelopment');
+
+    // Global Vars
+    var _pref = "mr",
+        _structure = "-structure/",
+        _data;
+
+    // Content Vars
+    var egHtml,
+        egData;
+
+    egHtml =
+        '<!DOCTYPE html>\n' +
+        '<html lang=\"en\">\n' +
+        '<head>\n' +
+        '    <meta charset=\"UTF-8\">\n' +
+        '    <title>Minimal Replace Example</title>\n' +
+        '    <style>\n' +
+        '        body {\n' +
+        '            background: #2e4052;\n' +
+        '            font-family: sans-serif;\n' +
+        '            color: #f9f9f9;\n' +
+        '        }\n' +
+        '        .card {\n' +
+        '            margin: 0 3em;\n' +
+        '            position: absolute;\n' +
+        '            top: 50%;\n' +
+        '            transform: perspective(1px) translateY(-50%);\n' +
+        '        }\n' +
+        '        a { color: #f9f9f9; }\n' +
+        '    </style>\n' +
+        '</head>\n' +
+        '<body>\n' +
+        '    <div class=\'card\'>\n' +
+        '        <h5>Hello</h5>\n' +
+        '        <h1>I\'m {{::name}}</h1>\n' +
+        '        <h2>from the {{::planet}}</h2>\n' +
+        '        <p><i>This is an example page of Minimal Replace, for more information, find us on <a href=\"{{::mr-link}}\">Github</a></i></p>\n' +
+        '    </div>\n' +
+        '    <script src=\"index.js\"></script>\n' +
+        '    <script src=\"https://arturguedes.github.io/MinimalReplace/js/minimalreplace.min.js\"></script>\n' +
+        '</body>\n' +
+        '</html>';
+
+    egData =
+        'mr_data = {\n' +
+        '    \"name\": \"Arthur Phillip Dent\",\n' +
+        '    \"planet\": \"Earth\",\n' +
+        '    \"mr-link\": \"https://github.com/ArturGuedes/MinimalReplace\"\n' +
+        '}';
+
+    // Common Methods
+    function error(err) {
+        console.log("Error: " + err);
+    }
+
+    function dialog(title, content) {
+        Dialogs.showModalDialog(
+            DefaultDialogs.DIALOG_ID_INFO, title, content);
+    }
+
+    function createFile(path, filename, content) {
+        var file = FileSystem.getFileForPath(path + filename);
+
+        FileUtils.writeText(file, content, true).done(function () {
+            //console.log("Text successfully updated");
+        }).fail(function (err) {
+            error(err.name);
+        });
+    }
+
+    // Core Methods
+    function init() {
+        var _fileName = DocumentManager.getCurrentDocument().file["name"],
+            _fileNameWE = FileUtils.getFilenameWithoutExtension(_fileName),
+            _pathProject = ProjectManager.getProjectRoot().fullPath,
+            _pathStructure = _pathProject + _pref + _structure;
+
+        brackets.fs.makedir(_pathStructure, 777);
+        createFile(_pathStructure, "example.html", egHtml);
+        createFile(_pathStructure, "example.js", egData);
+        dialog(
+            "Minimal Replace",
+            "<center><i>Folder and sample files successfully created in this project.</i>" +
+            "<p>For more information, find us on <a href=\"https://github.com/ArturGuedes/MinimalReplace/\">Github</a></p>"
+        );
+    }
+
+    function replace() {
+        //LiveDevelopment.reload();
+
+        if (DocumentManager.getCurrentDocument().file['parentPath'].includes(_pref)) {
+            var _fileName = DocumentManager.getCurrentDocument().file["name"],
+                _fileNameWE = FileUtils.getFilenameWithoutExtension(_fileName),
+                _pathProject = ProjectManager.getProjectRoot().fullPath,
+                _pathStructure = _pathProject + _pref + _structure,
+                _data,
+                _dataHTML,
+                fileData = FileSystem.getFileForPath(_pathStructure + _fileNameWE + '.js'),
+                fileHTML = FileSystem.getFileForPath(_pathStructure + _fileNameWE + '.html'),
+                fileFINAL = FileSystem.getFileForPath(_pathProject + _fileNameWE + '.html');
+
+            FileUtils.readAsText(fileData).done(function (rawText, readTimestamp) {
+                rawText = rawText.replace("mr_data = ", "");
+                _data = JSON.parse(rawText);
+                FileUtils.readAsText(fileHTML).done(function (rawText, readTimestamp) {
+                    _dataHTML = rawText;
+                    for (var prop in _data) {
+                        // Console test
+                        // console.log(prop + " = " + _data[prop]);
+                        _dataHTML = _dataHTML.split("{{::" + prop + "}}").join(_data[prop]);
+                    }
+                    // Remove "../"
+                    _dataHTML = _dataHTML.split("../").join("");;
+                    createFile(_pathProject, _fileNameWE + '.html', _dataHTML);
+                }).fail(function (err) {
+                    error(err.name);
+                });
+            }).fail(function (err) {
+                error(err.name);
+            });
+        }
+    }
+
+    // Menu
+    var Menu = Menus.addMenu('Minimal', 'arturguedes.minimal'),
+        cmm_init = "arturguedes.minimalreplace_init",
+        cmm_about = "arturguedes.minimalreplace_about";
+
+    CommandManager.register("Setup Minimal Replace structure", cmm_init, init);
+    //CommandManager.register("About", i_about, about);
+
+    Menu.addMenuItem(cmm_init, "Alt-Shift-R");
+    //Menu.addMenuDivider();
+    //Menu.addMenuItem(i_about);
+
+    // Save
+    $(DocumentManager).on('documentSaved', function (event, document) {
+        replace();
+    });
+
+});
