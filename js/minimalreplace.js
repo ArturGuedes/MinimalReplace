67e86bf499af7dd61f8aa13b9605e9ba1ae3e0e0
Artur Guedes <arturguedx@gmail.com>
2018-11-23 02:04:10 -0200

    Update CDN files

--- js/minimalreplace.js
+++ js/minimalreplace.js
@@ -5,7 +5,7 @@ var mr_data, _temp;
 _temp = document.documentElement.innerHTML;
 
 for (var prop in mr_data) {
-    _temp = _temp.replace(("{{::" + prop + "}}"), mr_data[prop]);
+    _temp = _temp.split("{{::" + prop + "}}").join(_data[prop]);
 };
 
 document.body.innerHTML = _temp;
