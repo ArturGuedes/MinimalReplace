380a78aa3c2e636f751a06295feb3afd8cfa9efc
Artur Guedes <arturguedx@gmail.com>
2018-11-23 02:07:53 -0200

    Fix mr_data error

--- js/minimalreplace.js
+++ js/minimalreplace.js
@@ -5,7 +5,7 @@ var mr_data, _temp;
 _temp = document.documentElement.innerHTML;
 
 for (var prop in mr_data) {
-    _temp = _temp.split("{{::" + prop + "}}").join(_data[prop]);
+    _temp = _temp.split("{{::" + prop + "}}").join(mr_data[prop]);
 };
 
 document.body.innerHTML = _temp;
